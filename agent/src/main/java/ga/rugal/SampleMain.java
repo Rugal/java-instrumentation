cc540c5779e98ff31e949f6e5e4cfd6854696881
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-03-02 15:13:23 +0800

    Add agentmain method

--- agent/src/main/java/ga/rugal/SampleMain.java
+++ agent/src/main/java/ga/rugal/SampleMain.java
@@ -16,10 +16,22 @@ public class SampleMain {
    * Pre main function.
    *
    * @param agentArgs agent argument
-   * @param inst instrumentation
+   * @param inst      instrumentation
    */
   public static void premain(final String agentArgs, final Instrumentation inst) {
     LOG.info("[Agent] In premain method");
+
+    inst.addTransformer(new SampleTransformer());
+  }
+
+  /**
+   * Attach to JVM after startup.
+   *
+   * @param agentArgs agent arguments
+   * @param inst      instrumentation
+   */
+  public static void agentmain(final String agentArgs, final Instrumentation inst) {
+    LOG.info("[Agent] In agentmain method");
 
     inst.addTransformer(new SampleTransformer());
   }
